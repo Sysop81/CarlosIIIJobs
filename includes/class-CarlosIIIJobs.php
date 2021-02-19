f51dc02261e6f6e8fd28ede7644a045969fbbe7a
Jose Ramon Lopez <jose.rlg27@gmail.com>
2021-02-19 08:02:57 +0100

    Creando un shortcode para listar ofertas de empleo

--- includes/class-CarlosIIIJobs.php
+++ includes/class-CarlosIIIJobs.php
@@ -141,6 +141,11 @@ class CarlosIIIJobs {
          */
         require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-CarlosIIIJobsWidgetSuscribe.php';
 		
+		/**
+         * The class responsible for defining shortcode.
+         */
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-CarlosIIIJobs-shortcode.php';
+		
 		$this->loader = new CarlosIIIJobs_Loader();
 	}
 
@@ -175,6 +180,10 @@ class CarlosIIIJobs {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
+		$plugin_shortcode = new CarlosIIIJobs_shortcode();
+
+        $this->loader->add_action( 'init', $plugin_shortcode, 'CarlosIIIJobs_shortcode_init' );
+
 	}
 
 	/**
@@ -190,7 +199,9 @@ class CarlosIIIJobs {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
+		
+		
+     
 	}
 
 	private function define_job_types() {
@@ -238,5 +249,7 @@ class CarlosIIIJobs {
 	public function get_version() {
 		return $this->version;
 	}
+
+
 
 }
